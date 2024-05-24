c85c1ea43267a6f400dc56853842411aaabcc27b
nangsontay <ngon.maithien05@hcmut.edu.vn>
2024-05-24 22:32:59 +0700

    minor fix stupid \r errors

--- main.cpp
+++ main.cpp
@@ -238,7 +238,7 @@ class TestStudyInPink {
 int main(int argc, char *argv[]) {
   cout << "Start program assignments 2 KTLT" << endl;
   cout << "Running test : ";
-  int start = 0, end = 10;
+  int start = 1, end = 1;
   string folderr = "testcase/output/output";
   // string folderr = "testcase/expect/expect";
   if (start == 0) {
